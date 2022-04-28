ea19163af227d1d537c6da1c8e76ab78fefe64f5
DanyKuzmenko <sh0k555@yandex.ru>
2022-04-28 22:02:10 +0300

    fix: fix auth message

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new ErrorUnauthorized('Необходима авторизаци'));
+    return next(new ErrorUnauthorized('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
